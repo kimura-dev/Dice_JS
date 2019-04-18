875014747e6ad429d87f197ab5cb50b931da043d
Scott Reyell <smreyell@gmail.com>
2019-04-17 19:28:06 -0700

    finished event listener on btn-roll

--- app.js
+++ app.js
@@ -14,12 +14,47 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
-
-dice = Math.floor(Math.random() * 6) + 1;
-// console.log(dice);
-
-
-document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '</em>';
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  //  Generate a random number
+  const dice = Math.floor(Math.random() * 6) + 1;
+  // Display the result
+  const diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // Update the round score IF the round number was NOT a 1!
+  if(dice !== 1){
+    // Add Score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    // Next Player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    
+    document.querySelector('.player-0-panel').classList.toggle('active');
+
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+  }
+
+})
